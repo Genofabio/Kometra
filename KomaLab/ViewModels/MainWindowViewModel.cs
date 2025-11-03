b89bc372c3b0345547e0ffae0c9e135bbb75b912
Genofabio <fabio.genovese.00@gmail.com>
2025-11-03 01:30:34 +0100

    Initial commit

---
+++ KomaLab/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace KomaLab.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ KomaLab/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KomaLab.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
